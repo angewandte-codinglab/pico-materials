0f80c893d316da11dbf2e52dde4503c1edb0e0c6
Ulrich Formann <m@ul.fo>
2025-10-13 13:13:17 +0200

    Add comment for Touchpad pin configuration
    Added a comment to specify the pin number for the Touchpad.

--- 3 - Sense/3_Touch/3_Touch.py
+++ 3 - Sense/3_Touch/3_Touch.py
@@ -2,7 +2,7 @@ from Touchpad import Touchpad
 from picozero import LED
 from time import sleep
 
-touch = Touchpad(1)
+touch = Touchpad(1) # Pin 0 - 11 on the Raspberry Pi Pico
 led   = LED(14)
 
 while True:
