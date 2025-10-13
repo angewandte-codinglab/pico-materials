13fa40ceddfdad88acc486b47279297411942f03
Ulrich Formann <m@ul.fo>
2025-10-13 13:13:29 +0200

    Clarify Touchpad pin assignment in code
    Added comment to clarify pin assignment for Touchpad.

--- 3 - Sense/3_Touch/3_Touch_Sense.py
+++ 3 - Sense/3_Touch/3_Touch_Sense.py
@@ -2,7 +2,7 @@ from Touchpad import Touchpad
 from picozero import LED, Pot
 from time import sleep
 
-touch = Touchpad(1)
+touch = Touchpad(1) # Pin 0 - 11 on the Raspberry Pi Pico
 led   = LED(14)
 pot   = Pot(26)
 
